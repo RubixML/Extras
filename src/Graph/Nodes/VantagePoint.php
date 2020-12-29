4980a8eceab6ec18e3700a9b27d13afa0c10d24a
Andrew DalPino <me@andrewdalpino.com>
2020-12-28 22:08:15 -0600

    Add Vantage Point node test

--- src/Graph/Nodes/VantagePoint.php
+++ src/Graph/Nodes/VantagePoint.php
@@ -5,6 +5,7 @@ namespace Rubix\ML\Graph\Nodes;
 use Rubix\ML\Datasets\Labeled;
 use Rubix\ML\Other\Helpers\Stats;
 use Rubix\ML\Kernels\Distance\Distance;
+use Rubix\ML\Graph\Nodes\Traits\HasBinaryChildren;
 
 use function Rubix\ML\argmax;
 
@@ -15,11 +16,33 @@ use function Rubix\ML\argmax;
  * @package     Rubix/ML
  * @author      Andrew DalPino
  */
-class VantagePoint extends Ball
+class VantagePoint implements BinaryNode, Hypersphere
 {
+    use HasBinaryChildren;
+
     /**
-     * Factory method to build a hypersphere by splitting the dataset into
-     * left and right clusters.
+     * The center or multivariate mean of the centroid.
+     *
+     * @var list<string|int|float>
+     */
+    protected $center;
+
+    /**
+     * The radius of the centroid.
+     *
+     * @var float
+     */
+    protected $radius;
+
+    /**
+     * The left and right splits of the training data.
+     *
+     * @var array{Labeled,Labeled}
+     */
+    protected $groups;
+
+    /**
+     * Factory method to build a hypersphere by splitting the dataset into left and right clusters.
      *
      * @param \Rubix\ML\Datasets\Labeled $dataset
      * @param \Rubix\ML\Kernels\Distance\Distance $kernel
@@ -66,5 +89,65 @@ class VantagePoint extends Ball
             Labeled::quick($leftSamples, $leftLabels),
             Labeled::quick($rightSamples, $rightLabels),
         ]);
+    }
+
+    /**
+     * @param list<string|int|float> $center
+     * @param float $radius
+     * @param array{Labeled,Labeled} $groups
+     */
+    public function __construct(array $center, float $radius, array $groups)
+    {
+        $this->center = $center;
+        $this->radius = $radius;
+        $this->groups = $groups;
+    }
+
+    /**
+     * Return the center vector.
+     *
+     * @return list<string|int|float>
+     */
+    public function center() : array
+    {
+        return $this->center;
+    }
+
+    /**
+     * Return the radius of the centroid.
+     *
+     * @return float
+     */
+    public function radius() : float
+    {
+        return $this->radius;
+    }
+
+    /**
+     * Return the left and right splits of the training data.
+     *
+     * @return array{Labeled,Labeled}
+     */
+    public function groups() : array
+    {
+        return $this->groups;
+    }
+
+    /**
+     * Does the hypersphere reduce to a single point?
+     *
+     * @return bool
+     */
+    public function isPoint() : bool
+    {
+        return $this->radius === 0.0;
+    }
+
+    /**
+     * Remove the left and right splits of the training data.
+     */
+    public function cleanup() : void
+    {
+        unset($this->groups);
     }
 }
