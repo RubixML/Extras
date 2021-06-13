cc42381ad2c9b51eacc728fe81ff7750d6194fab
Andrew DalPino <andrewdalpino@users.noreply.github.com>
2021-06-13 05:34:18 -0500

    Deduplicator (#11)
    * Initial commit
    
    * Add docs
    
    * Use function imports
    
    * Optimize Bloom filter
    
    * Use FNVa132 hash function

---
+++ docs/extractors/deduplicator.md
@@ -0,0 +1,30 @@
+<span style="float:right;"><a href="https://github.com/RubixML/Extras/blob/master/src/Extractors/Deduplicator.php">[source]</a></span>
+
+# Deduplicator
+Removes duplicate records from a dataset while the records are in flight. Deduplicator uses a Bloom filter under the hood to probabilistically identify records that have already been seen before.
+
+!!! note
+    Due to its probabilistic nature, Deduplicator may mistakenly drop unique records. The false positive rate depends on the size, number of hash functions, and the number of records in the Bloom filter.
+
+**Interfaces:** [Extractor](api.md)
+
+## Parameters
+| # | Name | Default | Type | Description |
+|---|---|---|---|---|
+| 1 | iterator | | Traversable | The base iterator. |
+| 2 | size | | int | The size of the Bloom filter. |
+| 3 | numHashes | 3 | int | The number of hash functions. |
+
+## Example
+```php
+use Rubix\ML\Extractors\Deduplicator;
+use Rubix\ML\Extractors\CSV;
+
+$extractor = new Deduplicator(new CSV('example.csv', true), 2048000, 3);
+```
+
+## Additional Methods
+Return the probability of mistakenly dropping a unique record.
+```php
+public falsePositiveRate() : float
+```
---
+++ src/Extractors/Deduplicator.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace Rubix\ML\Extractors;
+
+use Rubix\ML\BooleanArray;
+use Rubix\ML\Exceptions\InvalidArgumentException;
+use Generator;
+
+use function serialize;
+use function unpack;
+use function hash;
+use function exp;
+
+/**
+ * Deduplicator
+ *
+ * Removes duplicate records from a dataset while the records are in flight. Deduplicator uses a memory-efficient
+ * Bloom filter to probabilistically identify records that have already been seen before.
+ *
+ * @category    Machine Learning
+ * @package     Rubix/ML
+ * @author      Andrew DalPino
+ */
+class Deduplicator implements Extractor
+{
+    /**
+     * The base hash function.
+     *
+     * @var string
+     */
+    protected const HASH_FUNCTION = 'fnv1a32';
+
+    /**
+     * The base iterator.
+     *
+     * @var iterable<array>
+     */
+    protected iterable $iterator;
+
+    /**
+     * The size of the bloom filter.
+     *
+     * @var int
+     */
+    protected int $size;
+
+    /**
+     * The number of hash functions.
+     *
+     * @var int
+     */
+    protected int $numHashes;
+
+    /**
+     * A fixed size boolean array.
+     *
+     * @var \Rubix\ML\BooleanArray
+     */
+    protected \Rubix\ML\BooleanArray $bitmap;
+
+    /**
+     * The number of records that have been processed so far.
+     */
+    protected int $n = 0;
+
+    /**
+     * @param iterable<mixed[]> $iterator
+     * @param int $size
+     * @param int $numHashes
+     * @throws \Rubix\ML\Exceptions\InvalidArgumentException
+     */
+    public function __construct(iterable $iterator, int $size, int $numHashes = 3)
+    {
+        if ($size < 1) {
+            throw new InvalidArgumentException('Size must be'
+                . " greater than 1, $size given.");
+        }
+
+        if ($numHashes < 1) {
+            throw new InvalidArgumentException('Number of hashes'
+                . " must be greater than 1, $numHashes given.");
+        }
+
+        $this->iterator = $iterator;
+        $this->size = $size;
+        $this->numHashes = $numHashes;
+        $this->bitmap = new BooleanArray($size);
+    }
+
+    /**
+     * Return the probability of mistakenly dropping a unique record.
+     *
+     * @return float
+     */
+    public function falsePositiveRate() : float
+    {
+        $capacity = $this->n / $this->size;
+        
+        return (1.0 - exp(-$this->numHashes * $capacity)) ** $this->numHashes;
+    }
+
+    /**
+     * Return an iterator for the records in the data table.
+     *
+     * @return \Generator<mixed[]>
+     */
+    public function getIterator() : Generator
+    {
+        foreach ($this->iterator as $record) {
+            $token = serialize($record);
+
+            if (!$this->existsOrInsert($token)) {
+                yield $record;
+            }
+
+            ++$this->n;
+        }
+    }
+
+    /**
+     * Does a value exist in the Bloom filter? If so, return true or insert and return false.
+     *
+     * @param string $value
+     * @return bool
+     */
+    protected function existsOrInsert(string $value) : bool
+    {
+        $hashes = $this->hashes($value);
+
+        $exists = true;
+
+        foreach ($hashes as $hash) {
+            if (!$this->bitmap[$hash]) {
+                $this->bitmap[$hash] = true;
+
+                if ($exists) {
+                    $exists = false;
+                }
+            }
+        }
+
+        return $exists;
+    }
+
+    /**
+     * Return an array of hashes from a given string.
+     *
+     * @param string $value
+     * @return list<int>
+     */
+    protected function hashes(string $value) : array
+    {
+        $hashes = [];
+
+        for ($i = 0; $i < $this->numHashes; ++$i) {
+            $digest = hash(self::HASH_FUNCTION, "$i:$value");
+
+            /** @var int[] $bytes */
+            $bytes = unpack('S*', $digest);
+
+            $hash = 0;
+
+            if (PHP_INT_SIZE === 8) {
+                $hash |= $bytes[1] << 48;
+                $hash |= $bytes[2] << 32;
+                $hash |= $bytes[3] << 16;
+                $hash |= $bytes[4];
+            } else {
+                $hash |= $bytes[1] << 16;
+                $hash |= $bytes[2];
+            }
+
+            $hash %= $this->size;
+
+            $hashes[] = $hash;
+        }
+
+        return $hashes;
+    }
+}
